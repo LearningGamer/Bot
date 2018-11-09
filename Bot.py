30d44c2459aaa973199835789cd126901da296b0
LearningGamer <woodendoor101@gmail.com>
2018-11-09 11:53:06 -0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -4,17 +4,19 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 
 
+
+
 bot = commands.Bot(command_prefix='$')
 
 @bot.command(pass_context=True)
-async def test(ctx):
-  msg = 'Hi!'
-  await bot.say(msg)
-
+async def test(msg):
+  await bot.say('Hi.')
+  await bot.delete_message(msg.message)
 
 @bot.event
 async def  on_ready():
     print("I'm in")
     print(bot.user.name)
 
-bot.run(os.environ["token"])
+token = os.environ.get("DISCORD_BOT_SECRET")
+bot.run(token)
