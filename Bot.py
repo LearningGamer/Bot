c8bbb82070fe946e7912dc849842c02cca2c9710
LearningGamer <woodendoor101@gmail.com>
2018-11-09 15:36:32 -0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -10,16 +10,16 @@ async def hel(ctx,):
     await bot.send_message(ctx.message.author,  '`Commands:\n $hel = Bot sends a pm of a list of commands to the user.\n $test = Test to see if bot is responsive.`')
     
 @bot.command(pass_context=True)
+@commands.has_role('Bot Owner')
 async def spam(ctx):
   spammsg = 'LOLOLOLOLOLOLOLOL '
   await bot.say(spammsg *100)
-  await bot.delete_message(msg.message)
 
 
 @bot.command(pass_context=True)
-async def test(ctx, msg):
-  await bot.say('Test works! Message Deleted. Bot is responsive.')
-  await bot.delete_message(msg.message)
+async def test(ctx):
+  await bot.say('Test works! Bot is responsive.')
+  await bot.delete_message(ctx)
 
 @bot.event
 async def  on_ready():
