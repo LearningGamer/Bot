016e702098ce86d4bbf286158db44bc749913bab
LearningGamer <woodendoor101@gmail.com>
2018-11-09 14:42:20 -0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -3,14 +3,18 @@ import os
 from discord.ext import commands
 from discord.ext.commands import Bot
 
-
-
-
 bot = commands.Bot(command_prefix='$')
 
 @bot.command(pass_context=True)
-async def test(msg):
-  await bot.say('Hi.')
+async def hel(ctx,): 
+    await bot.send_message(ctx.message.author,  'Commands:\n $hel = Bot sends a pm of a list of commands to the user.\n $test = Test to see if bot is responsive.')
+    
+
+
+
+@bot.command(pass_context=True)
+async def test(ctx, msg):
+  await bot.say('Test works! Message Deleted. Bot is responsive.')
   await bot.delete_message(msg.message)
 
 @bot.event
@@ -18,4 +22,5 @@ async def  on_ready():
     print("I'm in")
     print(bot.user.name)
 
-bot.run(os.environ["token"])
+token = os.environ.get("DISCORD_BOT_SECRET")
+bot.run(token)
