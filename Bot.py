f5fe4d9c027bc6b3705ef9b04fdca24cdb9c381e
LearningGamer <woodendoor101@gmail.com>
2018-11-13 18:58:52 -0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -6,7 +6,7 @@ bot = commands.Bot(command_prefix='$')
 
 @bot.command(pass_context=True)
 async def hel(ctx,): 
-    await bot.send_message(ctx.message.author,  '`You must create a role called Bot Owner for this bot to work properly. \n Commands:\n $hel = Bot sends a pm of a list of commands to the user.\n $test = Test to see if bot is responsive. \n $spam = Spams chat, must have Bot Owner role to use the command.`')
+    await bot.send_message(ctx.message.author,  '`You must create a role called Bot Owner for this bot to work properly. \n Commands:\n $hel = Bot sends a pm of a list of commands to the user.\n $test = Test to see if bot is responsive. \n $spam = Spams chat, must have Bot Owner role to use the command. \n $game = Sets the game the bot is to display, must have Bot Owner role.`')
     await bot.say('Sent a DM containing a list of commands')
     await bot.delete_message(ctx.message)
 
