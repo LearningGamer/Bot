459d1cfe142c9189852b93c19811d1c9ce795a79
LearningGamer <woodendoor101@gmail.com>
2018-11-28 16:00:55 -0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -6,9 +6,21 @@ bot = commands.Bot(command_prefix='$')
 
 @bot.command(pass_context=True)
 async def hel(ctx,): 
-    await bot.send_message(ctx.message.author,  '`You must create a role called Bot Owner for this bot to work properly. \n Commands:\n $hel = Bot sends a pm of a list of commands to the user.\n $test = Test to see if bot is responsive. \n $spam = Spams chat, must have Bot Owner role to use the command. \n $game = Sets the game the bot is to display, must have Bot Owner role.`')
+    await bot.send_message(ctx.message.author,  '`You must create a role called Bot Owner for this bot to work properly. \n Commands:\n $hel = Bot sends a pm of a list of commands to the user.\n $test = Test to see if bot is responsive. \n $spam = Spams chat, must have Bot Owner role to use the command. \n $game = Sets the game the bot is to display, must have Bot Owner role. \n $watching = Sets the thing the bot is to display as Watching, must have Bot Owner role. \n $listening Sets the thing the bot is to display as listening to, must have the Bot Owner role.`')
     await bot.say('Sent a DM containing a list of commands')
     await bot.delete_message(ctx.message)
+    
+@bot.command(pass_context=True)
+@commands.has_role('Bot Owner')
+async def watching(ctx,*,status):
+    await bot.change_presence(game=discord.Game(name=status,type=3))
+    await bot.send_message(ctx.message.channel,"Bot status changed to {}".format(status))
+    
+@bot.command(pass_context=True)
+@commands.has_role('Bot Owner')
+async def listening(ctx,*,status):
+    await bot.change_presence(game=discord.Game(name=status,type=2))
+    await bot.send_message(ctx.message.channel,"Bot status changed to {}".format(status))
 
 @bot.command(pass_context=True)
 @commands.has_role('Bot Owner')
