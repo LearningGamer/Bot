28ad835c00c11978b346e955597c64636239bd36
LearningGamer <woodendoor101@gmail.com>
2018-12-06 11:16:31 -0600

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -3,10 +3,11 @@ import os
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='$')
+bot.remove_command('help')
 
 @bot.command(pass_context=True)
-async def hel(ctx,): 
-    await bot.send_message(ctx.message.author,  '`You must create a role called Bot Owner for this bot to work properly. \n Commands:\n $hel = Bot sends a pm of a list of commands to the user.\n $test = Test to see if bot is responsive. \n $spam = Spams chat, must have Bot Owner role to use the command. \n $game = Sets the game the bot is to display, must have Bot Owner role. \n $watching = Sets the thing the bot is to display as Watching, must have Bot Owner role. \n $listening Sets the thing the bot is to display as listening to, must have the Bot Owner role.`')
+async def help(ctx,): 
+    await bot.send_message(ctx.message.author,  '`You must create a role called Bot Owner for this bot to work properly. \n Commands:\n $help = Bot sends a pm of a list of commands to the user.\n $test = Test to see if bot is responsive. \n $spam = Spams chat, must have Bot Owner role to use the command. \n $game = Sets the game the bot is to display, must have Bot Owner role. \n $watching = Sets the thing the bot is to display as Watching, must have Bot Owner role. \n $listening Sets the thing the bot is to display as listening to, must have the Bot Owner role.`')
     await bot.say('Sent a DM containing a list of commands')
     await bot.delete_message(ctx.message)
     
